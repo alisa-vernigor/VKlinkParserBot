907fe513ec882963e50479968f80a3174a072a5a
SphericalPotatoInVacuum <a@bogdanov.co>
2020-08-24 02:20:11 +0400

    Handle telegram faults

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import vk_api
 import telegram
 from telegram import InputMediaPhoto, InputMediaVideo, InputMedia
 from telegram.ext import MessageHandler, Updater, Dispatcher, CommandHandler, Filters
+from telegram.error import BadRequest
 from settings import service_key, test_id, token
 from my_parser import Parser
 import os
@@ -11,11 +12,13 @@ updater = Updater(token=token, use_context=True)
 dispatcher = updater.dispatcher
 parser = Parser(service_key)
 
+
 def start_help(update, context):
     context.bot.send_message(
         chat_id=update.message.chat_id,
         text='По всем вопросам обращаться к @sphericalpotatoinvacuum или @allisyonok'
     )
+
 
 def links(update, context):
     try:
@@ -26,15 +29,22 @@ def links(update, context):
                 album.append(InputMediaVideo(media=url))
             elif url_type == Parser.UrlType.PHOTO:
                 album.append(InputMediaPhoto(media=url))
-        print(album)
-        context.bot.send_media_group(chat_id=update.message.chat_id, media=album)
+        context.bot.send_media_group(
+            chat_id=update.message.chat_id, media=album)
+    except BadRequest as e:
+        context.bot.send_message(chat_id=update.message.chat_id,
+                                 text="Telegram servers couldn't reach some "
+                                 "of the files :(")
     except Exception as e:
         pass
 
 
 start_handler = CommandHandler('start', start_help)
 help_handler = CommandHandler('help', start_help)
-links_handler = MessageHandler(filters=Filters.regex(r'(https://)?vk.com(.)*'), callback=links)
+links_handler = MessageHandler(
+    filters=Filters.regex(r'(https://)?vk.com(.)*'),
+    callback=links
+)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(help_handler)
--- my_parser.py
+++ my_parser.py
@@ -6,26 +6,26 @@ import uuid
 import shutil
 from enum import Enum, auto
 
+
 class Parser:
     class UrlType(Enum):
         VIDEO = auto()
         PHOTO = auto()
 
-    def __init__(self, service_key):  
+    def __init__(self, service_key):
         vk_session = vk_api.VkApi(token=service_key)
         self.vk = vk_session.get_api()
 
     def process_player_link(self, player_link) -> None:
         soup = BeautifulSoup(player_link, 'html.parser')
-        print(soup.video.find_all('source'))
-        url =  soup.video.find_all('source')[1]["src"]
+        url = soup.video.find_all('source')[1]["src"]
         if url.find('?') == -1:
             return url
         return url[:url.find('?')]
 
-
     def parse_video(self, video_obj):
-        video = self.vk.video.get(videos=f"{video_obj['owner_id']}_{video_obj['id']}_{video_obj['access_key']}")
+        video = self.vk.video.get(
+            videos=f"{video_obj['owner_id']}_{video_obj['id']}_{video_obj['access_key']}")
         return self.process_player_link(
             requests.get(video["items"][0]["player"]).content)
 
@@ -44,7 +44,6 @@ class Parser:
         return urls
 
     def process_id(self, wall_id):
-        print("I am in process_id")
         try:
             obj = self.vk.wall.getById(posts=wall_id, fields="attachments")
             urls = self.parse_attachments(obj[0]["attachments"])
@@ -57,7 +56,5 @@ class Parser:
         index = link.find("wall")
         if index == -1:
             return
-        print(index)
         urls = self.process_id(link[index + 4:])
         return urls
-
